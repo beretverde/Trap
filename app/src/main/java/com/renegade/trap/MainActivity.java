45503339ce2674ff2bcd0ce74ad36e69ee11d3f9
brentjacox <gitkraken@smoothlake.com>
2017-07-04 20:23:01 -0600

    remove file provider

--- app/src/main/java/com/renegade/trap/MainActivity.java
+++ app/src/main/java/com/renegade/trap/MainActivity.java
@@ -12,7 +12,6 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.NavigationView;
 import android.support.design.widget.Snackbar;
 import android.support.design.widget.TextInputEditText;
-import android.support.v4.content.FileProvider;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
