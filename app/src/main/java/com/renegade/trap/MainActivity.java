d6dd9f90e2684f7685cbb24d3a5937e30260d441
Tyler <tylerjacox@gmail.com>
2017-07-08 10:12:46 -0600

    pdf

--- app/src/main/java/com/renegade/trap/MainActivity.java
+++ app/src/main/java/com/renegade/trap/MainActivity.java
@@ -30,6 +30,7 @@ import android.widget.ImageView;
 import android.widget.Spinner;
 import android.widget.Toast;
 
+import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
@@ -145,10 +146,48 @@ public class MainActivity extends AppCompatActivity
 
 
 
-                email.setType("text/plain");
+                PdfDocument document = new PdfDocument();
+
+                // crate a page description
+                PdfDocument.PageInfo pageInfo = new PdfDocument.PageInfo.Builder(300, 300, 1).create();
+
+                // start a page
+                PdfDocument.Page page = document.startPage(pageInfo);
+
+                // draw something on the page
+
+                view.draw(page.getCanvas());
+
+                // finish the page
+                document.finishPage(page);
+
+                ByteArrayOutputStream os = new ByteArrayOutputStream();
+                try {
+                    document.writeTo(os);
+                    document.close();
+                    os.close();
+                } catch (IOException e) {
+                    throw new RuntimeException("Error generating file", e);
+                }
+
+
+                // Assuming it may go via eMail:
+
+
+
+
+
+                email.setType("text/html");
+
+//                email.setType("text/plain");
                 email.putExtra(Intent.EXTRA_EMAIL, new String[] {"gary@renegadeoil.net"});
                 email.putExtra(Intent.EXTRA_SUBJECT, locName+", "+cityName+", #"+workId);
 /*                email.putExtra(Intent.EXTRA_TEXT, "Default Template");*/
+
+                email.putExtra(
+                        getClass().getPackage().getName() + "." + "SendPDF",
+                        os.toByteArray());
+
                 ArrayList<Uri> photos = new ArrayList<>();
                 if (photoUri1 != null) {
                     photos.add(photoUri1);
@@ -170,31 +209,10 @@ public class MainActivity extends AppCompatActivity
                 } catch (ActivityNotFoundException ex) {
                     Toast.makeText(getApplicationContext(), "There are no email clients installed.", Toast.LENGTH_LONG).show();
                 }
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+//                        .setAction("Action", null).show();
 
-                PdfDocument document = new PdfDocument();
 
-                // crate a page description
-                PdfDocument.PageInfo pageInfo = new PdfDocument.PageInfo.Builder(new Rect(0, 0, 100, 100), 1).create();
-
-                // start a page
-                PdfDocument.Page page = document.startPage(pageInfo);
-
-                // draw something on the page
-                View content = city;
-                content.draw(page.getCanvas());
-
-                // finish the page
-                document.finishPage(page);
- . . .
-                // add more pages
- . . .
-                // write the document content
-                document.writeTo(getOutputStream());
-
-                // close the document
-                document.close();
 
             }
         });
